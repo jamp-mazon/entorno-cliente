acb01a8f43e94306f5049fc383466e09feae3b21
Jose Antonio Mazón <joseantoniomazonperez@gmail.com>
2025-10-16 18:44:28 +0200

    echos nuevos ejercicio hasta el 8

---
+++ DOM/ejercicios/ejercicios_consolidacion_dom(1-15)/script.js
@@ -0,0 +1,68 @@
+//Ejercicio 1
+let recuadro_borde=document.getElementById("borde_recuadro");
+let bton_recuadro=document.getElementById("boton_borde");
+bton_recuadro.addEventListener("click",function () {
+    recuadro_borde.style.border="3px solid red";
+})
+//Ejercicio 2
+let area_texto=document.getElementById("area_texto");
+//let contenido_texto=area_texto.value
+let pre=document.getElementById("pre_funciona");
+let btn_text_area=document.getElementById("btn_text_area");
+btn_text_area.addEventListener("click",function () {
+    pre.textContent=area_texto.value;
+})
+//Ejercicio 3
+let input=document.getElementById("input");
+let p=document.getElementById("parrafo_escrito");
+
+input.addEventListener("input",function () {
+    
+    p.textContent=`La cantidad de letras es:${input.value.length} letras`;
+})
+//Ejercicio 4
+let imagen=document.getElementById("imagen");
+let aumentar=document.getElementById("aumentar");
+let tamaño=150;
+let disminuir=document.getElementById("disminuir");
+aumentar.addEventListener("click",function () {
+    imagen.style.width=`${tamaño++}px`;
+});
+disminuir.addEventListener("click",function () {
+    imagen.style.width=`${tamaño--}px`; 
+})
+//Ejercicio 5
+let bton_generar=document.getElementById("bton_generar");
+bton_generar.addEventListener("click",function () {
+    let h2=document.createElement("h2");
+    h2.textContent="Nuevo titulo añadido";
+    document.body.appendChild(h2);
+})
+//Ejercicio 6
+let pes=document.querySelectorAll(".parrafo")
+let bton_borrar=document.getElementById("bton_borrar");
+bton_borrar.addEventListener("click",function () {
+    for (let i = 0; i < pes.length; i++) {
+        pes[i].remove();
+    }
+})
+//Ejercicio 7
+let div_colores=document.getElementById("div_colores");
+let bton_rojo=document.getElementById("bton_rojo");
+let bton_verde=document.getElementById("bton_verde");
+let bton_azul=document.getElementById("bton_azul");
+
+bton_rojo.addEventListener("click",function () {
+    div_colores.style.backgroundColor="red";
+    div_colores.textContent="texto Rojo";
+})
+bton_verde.addEventListener("click",function () {
+    div_colores.style.backgroundColor="green";
+    div_colores.textContent="texto verde";
+})
+bton_azul.addEventListener("click",function () {
+    div_colores.style.backgroundColor="blue";
+    div_colores.textContent="texto azul";
+})
+//Ejercicio 8
+
