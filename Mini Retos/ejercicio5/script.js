86bb6f7b388f944824fc8d7c597724e27e16b0b2
Jose Antonio Mazón <joseantoniomazonperez@gmail.com>
2025-10-22 20:09:41 +0200

    no funciona ej5

--- Mini Retos/ejercicio5/script.js
+++ Mini Retos/ejercicio5/script.js
@@ -4,5 +4,13 @@ let btnComprobar=getElementById("btnComprobar");
 let resultado=getElementById("resultado");
 
 btnComprobar.addEventListener("click",function () {
-    
+    let seleccionCPU=cpu.value;
+    let seleccionPlaca=placa.value;
+
+    if (seleccionCPU===seleccionPlaca) {
+        resultado.textContent="Es compatible";
+    }
+    else{
+        resultado.textContent="No es compatible";
+    }
 })
