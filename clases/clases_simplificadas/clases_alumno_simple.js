727934802ef50a3d9efa7491f8dd46b91d21e1a4
Jose Antonio Mazón <joseantoniomazonperez@gmail.com>
2025-11-04 20:45:44 +0100

    comienzo ejercicios

---
+++ clases/clases_simplificadas/clases_alumno_simple.js
@@ -0,0 +1,7 @@
+btn_crear=document.getElementById("e1-crear");
+btn_incrementar=document.getElementById("e1-inc");
+btn_decrementar=document.getElementById("e1-dec");
+btn_reset=document.getElementById("e1-reset");
+class Contador{
+    constructor()
+}
