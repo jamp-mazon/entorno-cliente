eeb53e0dc3f5c575f4f05dce8c4f2ac37e1930a3
jamp-mazon <joseantoniomazonperez@gmail.com>
2025-12-02 21:01:04 +0100

    extensiones

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,25 @@
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import pluginJs from "@eslint/js";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",   // CLAVE para evitar errores de import/export
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      "no-var": "error",
+      "prefer-const": "warn",
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      eqeqeq: ["warn", "always"],
+      "no-console": "off"
+    },
+    extends: [pluginJs.configs.recommended]
+  }
 ]);
