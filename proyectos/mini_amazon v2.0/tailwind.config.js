1919cff27006e233ce230d21c8b8a52ee90e6876
jamp-mazon <joseantoniomazonperez@gmail.com>
2025-12-16 12:34:14 +0100

    falta hacer el responsive en clase

---
+++ proyectos/mini_amazon v2.0/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        marca: {
+          500: "#6366f1",
+          600: "#4f46e5",
+          700: "#4338ca"
+        }
+      }
+    }
+  }
+};
